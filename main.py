e63cdefd75ba7f8eb4f87de21d4df7e552a2fe7d
elseginy10 <100611245+elseginy10@users.noreply.github.com>
2023-12-17 20:34:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ import json
 with open("data/queue.json", "r") as file:
     queue = json.load(file)
 
+st.title(':rainbow[Mahmoud amgad elseginy] \n')
+st.title(':rainbow[4211027]')
 
-enqueue_input_container, enqueue_btn_container = st.columns(2)
+enqueue_input = st.number_input("Enter a number:", value=0)
 
-with enqueue_input_container:
-    enqueue_input = st.number_input("Enter a number:", value=0)
+enqueue_btn_container,dequeue_container, is_empty_btn_container = st.columns(3)
 
 with enqueue_btn_container:
     enqueue_btn = st.button("enqueue")
 
 if enqueue_btn and enqueue_input != "":
     queue.append(enqueue_input)
-
-dequeue_container, is_empty_btn_container = st.columns(2)
 
 with dequeue_container:
     dequeue_btn = st.button("dequeue")
@@ -28,16 +27,15 @@ with is_empty_btn_container:
 if dequeue_btn:
     queue.pop(0)
 
-st.write(queue)
+st.write(' <- '.join(map(str, queue)))
 
 if is_empty_btn:
-    st.write(f"The queue is {'not empty' if queue else 'empty'}")
-
     if not queue:
+        st.error("The queue is empty")
         st.balloons()
+    else:
+        st.success("The queue is not empty")
+        st.snow()
     
-
-
-
 with open("data/queue.json", "w") as file:
     json.dump(queue, file)
